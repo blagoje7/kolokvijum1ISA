c2f80b38af47071fa6117bd3f82f7db48f885041
blagoje7 <stojsavlejvic.blagoje@gmail.com>
2022-04-04 09:39:40 +0200

    Init

---
+++ src/test/java/kolokvijum/prvikolokvijum/PrviKolokvijumApplicationTests.java
@@ -0,0 +1,13 @@
+package kolokvijum.prvikolokvijum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrviKolokvijumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
